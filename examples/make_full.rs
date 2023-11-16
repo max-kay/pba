bc0ce8cc3151bf35773722faaf3632ef224ee6dd
Max Krummenacher <maxkrummenacher@bluewin.ch>
2023-11-16 16:03:06 +0100

    make_full now actually full

--- examples/make_full.rs
+++ examples/make_full.rs
@@ -1,7 +1,7 @@
 use pba::Model;
 
 fn main() {
-    let model = Model::<4>::new(0.0, 0.0, 0.75, None);
+    let model = Model::<4>::new(0.0, 0.0, 1.0, None);
     model.write_to_cif("mmcif/full.mmcif").unwrap();
     model.safe_to_txt("out.txt").unwrap();
 }
