217b5d232058b2ed90f60dfa3588afda47c7a1a4
Max Krummenacher <maxkrummenacher@bluewin.ch>
2023-11-17 13:56:42 +0100

    added diffraction pattern maps

--- python/data_analysis.py
+++ python/data_analysis.py
@@ -75,6 +75,40 @@ class Diffraction:
         """
         return np.isnan(self.intensities).all()
 
+    def get_averaged_section(self, value) -> np.ndarray:
+        """
+        get a section at value where all directions +- are averaged
+        """
+        # TODO should I apply mirror and rotational symmetries?
+        # this pattern is inversion symmetric. Can I use this?
+        sum = self.get_h_section(value)
+        sum += self.get_h_section(-value)
+        sum += self.get_k_section(value)
+        sum += self.get_k_section(-value)
+        sum += self.get_l_section(value)
+        sum += self.get_l_section(-value)
+        return sum / 6
+
+    def get_h_section(self, h_value) -> np.ndarray:
+        """
+        get a section of the diffraction pattern at the specified h_value
+        """
+        int_h_value = int(round(h_value * self.super_cells[0]))
+        int_h_max, _int_k_max, _int_l_max = np.round(
+            self.frac_hkl_max * self.super_cells
+        ).astype(np.int32)
+        return self.intensities[int_h_max + int_h_value, :, :]
+
+    def get_k_section(self, k_value) -> np.ndarray:
+        """
+        get a section of the diffraction pattern at the specified l_value
+        """
+        int_k_value = int(round(k_value * self.super_cells[1]))
+        _int_h_max, int_k_max, _int_l_max = np.round(
+            self.frac_hkl_max * self.super_cells
+        ).astype(np.int32)
+        return self.intensities[:, int_k_max + int_k_value, :]
+
     def get_l_section(self, l_value) -> np.ndarray:
         """
         get a section of the diffraction pattern at the specified l_value
@@ -240,14 +274,12 @@ def analyze_mmcif(run: str, name: str, supercells: str):
 def analyze_run(run: str):
     """
     calculates all diffraction patterns of a run and saves them as .h5 files in the yell format
-    additionally saves the hk0 section as pngs
     """
     with open(f"out/csv/{run}.csv", mode="r", encoding="utf8") as file:
         file.readline()
         supercells = int(file.readline().split(" ")[0])
     supercells = np.array([supercells] * 3)
     os.makedirs(f"out/h5/{run}", exist_ok=True)
-    os.makedirs(f"out/hk0/{run}", exist_ok=True)
     names = get_file_names(run)
     lenght = len(names)
     for i, name in enumerate(names):
@@ -287,7 +319,6 @@ def analyze_run_parallel(run: str):
     os.makedirs(f"out/hk0/{run}", exist_ok=True)
     names = get_file_names(run)
 
-
     num_processes = multiprocessing.cpu_count()
     pool = multiprocessing.Pool(processes=num_processes)
     args_list = [(run, name, supercells) for name in names]
@@ -295,5 +326,113 @@ def analyze_run_parallel(run: str):
     pool.close()
     pool.join()
 
-if __name__ == "__main__":
-    analyze_run_parallel("2023-11-16_17-01")
+
+def make_map_from_yells(
+    run: str,
+    value: float,
+    energy_values: int | None = None,
+    temp_values: int | None = None,
+):
+    temps, energies = get_sorted_energies_and_temps(run)
+
+    t_step = 1
+    if temp_values:
+        t_step = len(temps) // temp_values
+    temps = temps[::t_step]
+
+    energy_step = 1
+    if energy_values:
+        energy_step = len(energies) // energy_values
+    energies = energies[::energy_step]
+
+    diffraction = Diffraction.open_yell(
+        f"out/h5/{run}/j_{energies[-1]}_t_{temps[-1]}.h5"
+    )
+    w, h = find_biggest_non_nan_square(diffraction.get_averaged_section(value)).shape
+    del diffraction
+    assert w == h
+    img = np.empty((h * len(temps), w * len(energies)), dtype=np.float64)
+    img[:] = np.nan
+
+    for x, j in enumerate(energies):
+        for y, temp in enumerate(temps):
+            try:
+                diffraction = Diffraction.open_yell(f"out/h5/{run}/j_{j}_t_{temp}.h5")
+                img[
+                    y * h : (y + 1) * h,
+                    x * w : (x + 1) * w,
+                ] = find_biggest_non_nan_square(diffraction.get_averaged_section(value))
+                del diffraction
+            except FileNotFoundError:
+                pass
+    os.makedirs(f"out/maps/{run}", exist_ok=True)
+    img = np.log(img)
+    plt.imsave(
+        f"out/maps/{run}/hk{value}.png",
+        img,
+        cmap="gray",
+        origin="upper",
+        vmin=np.nanmin(img),
+        vmax=np.nanmax(img),
+    )
+
+
+def get_sorted_energies_and_temps(run):
+    """
+    returns the sorted temperatures and Js aviable for this run
+    """
+    temps = []
+    energies = []
+    for name in get_file_names(run):
+        _, j, _, t = name.split("_")
+        if t not in temps:
+            temps.append(t)
+        if j not in energies:
+            energies.append(j)
+
+    f_temps = list(zip([float(temp) for temp in temps], temps))
+    f_temps.sort(key=lambda tup: tup[1], reverse=True)
+    f_energies = list(zip([float(j) for j in energies], energies))
+    f_energies.sort(key=lambda tup: tup[0])
+    temps = [tup[1] for tup in f_temps]
+    energies = [tup[1] for tup in f_energies]
+    return temps, energies
+
+
+def shrink_to_fit_non_nan(array: np.ndarray) -> np.ndarray:
+    """
+    takes an array which has a border of NaN values surrounding the concrete values
+    and returns an array shrunk to the size such that all non nan values are contained and the shape
+    of the values is preserved
+    """
+    non_nan_indices = np.argwhere(~np.isnan(array))
+    print(non_nan_indices)
+    (min_row, min_col), (max_row, max_col) = non_nan_indices.min(
+        0
+    ), non_nan_indices.max(0)
+
+    # Extract the inner shape preserving the contents
+    return array[min_row : max_row + 1, min_col : max_col + 1]
+
+
+def find_biggest_non_nan_square(array: np.ndarray) -> np.ndarray:
+    """
+    shrinks the array to the biggest possible square that only contains non nan values
+    assumes the nan values are 4m symmetric and the shape formed by the non nan vallues is convex
+    """
+    w, h = array.shape
+    assert w == h
+    offset = 0
+    for i in range(w):
+        if not np.isnan(array[i, i]):
+            offset = i
+            break
+    return array[offset : w - offset, offset : w - offset]
+
+
+# if __name__ == "__main__":
+#     # run = "2023-11-16_16-54"
+#     run = "2023-11-16_17-01"
+#     # analyze_run_parallel(run)
+#     for value in range(10):
+#         make_map_from_yells(run, value)
